aacfb415781c2b3b536482f0593ba2beccc7d18a
Andrew Bird <andrew.bird@vesparum.com>
2019-04-20 11:32:30 -0500

    'wip'

--- Lesson03/Activities/fibonacci.py
+++ Lesson03/Activities/fibonacci.py
@@ -12,4 +12,11 @@ def fibonacci_recursive(n):
         return n
     else:
         return fibonacci_recursive(n - 2) + fibonacci_recursive(n - 1)
-    
+   
+stored = {0: 0, 1: 1}  # We set the first 2 terms of the Fibonacci sequence here.
+def fibonacci_dynamic(n):
+    if n in stored:
+        return stored[n]
+    else:
+        stored[n] = fibonacci_dynamic(n - 2) + fibonacci_dynamic(n - 1)
+        return stored[n]
