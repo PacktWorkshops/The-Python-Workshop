7fcc9897d911bba182bf372efddf0961ba63367a
Adrianpackt <packtadrian@gmail.com>
2019-07-16 19:03:48 +0530

    Updated codes

--- Lesson01/Activity01/Activity01.py
+++
@@ -1,8 +0,0 @@
-Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Inte
-l)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> x = 14
->>> x += 1
->>> (x/5) ** 2
-9.0
->>>
