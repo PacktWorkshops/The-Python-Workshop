4009dbe6eb1616dbead8cb12ad3593af67733745
Adrianpackt <packtadrian@gmail.com>
2019-08-21 20:18:04 +0530

    Updated folders

---
+++ Chapter08/Activity22/Activity22.py
@@ -0,0 +1,17 @@
+DEFAULT_INITIAL_BASKET = ["orange", "apple"]
+
+def create_picnic_basket(healthy, hungry, initial_basket=DEFAULT_INITIAL_BASKET):
+    basket = initial_basket
+    if healthy:
+        basket.append("strawberry")
+    else:
+        basket.append("jam")
+
+    if hungry:
+        basket.append("sandwich")
+    return basket
+
+# Reproducer
+#print("First basket:", create_picnic_basket(True, False))
+#print("Second basket:", create_picnic_basket(False, True, ["tea"]))
+#print("Third basket:", create_picnic_basket(True, True))
---
+++ Chapter08/Exercise112/Exercise112.py
@@ -0,0 +1,35 @@
+"""Adjusts the salary rise of an employ"""
+
+def _manager_adjust(salary, rise):
+    if rise < 0.10:
+        # We need to keep managers happy.
+        return 0.10
+
+    if salary >= 1_000_000:
+        # They are making enough already.
+        return rise - 0.10
+
+
+def calculate_new_salary(salary, promised_pct, is_manager, is_good_year):
+    rise = promised_pct
+
+    # remove 10% if it was a bad year
+    if not is_good_year:
+        rise -= 0.01
+    else:
+        pass
+
+    # managers have a special adjust
+    if is_manager:
+        rise = _manager_adjust(salary, rise)
+
+    # Extra bonus for people with high rises
+    if rise >= 0.20:
+        rise = rise + 0.10
+
+    salary_increase = salary * rise
+    return int(salary + salary_increase)
+
+
+rose_salary = calculate_new_salary(1_000_000, 0.30, True, True)
+print("Rose's salary will be:", rose_salary)
