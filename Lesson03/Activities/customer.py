6685d4985609641352b2edfeaa79fa82a8319d1c
Adrianpackt <packtadrian@gmail.com>
2019-10-08 15:20:53 +0530

    Updated files

--- Lesson03/Activities/customer.py
+++
@@ -1,6 +0,0 @@
-def format_customer(first, last, location=None):
-    full_name = '%s %s' % (first, last)
-    if location:
-        return '%s (%s)' % (full_name, location)
-    else:
-        return full_name
