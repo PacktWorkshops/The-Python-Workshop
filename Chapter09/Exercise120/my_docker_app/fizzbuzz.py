7da4b783c8bbc408d19e47c6f036217f3f744cd6
Rutu2709 <35215364+Rutu2709@users.noreply.github.com>
2020-06-25 09:27:48 +0530

    Chapters 9-11

--- Chapter09/Exercise120/my_docker_app/fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for num in range(1,101):
-    string = ""
-    if num % 3 == 0:
-        string = string + "Fizz"
-    if num % 5 == 0:
-        string = string + "Buzz"
-    if num % 5 != 0 and num % 3 != 0:
-        string = string + str(num)
-    print(string)
